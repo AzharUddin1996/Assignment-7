5128c904bd6e9e058d162579230ff0e2581752e9
AZHAR <a.uddin4321@gmail.com>
2024-06-07 17:03:28 +0600

    assignment-7 load data and creat fake dat

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Courses from "./components/Courses/Courses"
+
+
+function App() {
+
+  return (
+    <>
+      <h3 className="text-5xl text-center mt-5">Course registration</h3>
+      <Courses></Courses>
+    </>
+  )
+}
+
+export default App
